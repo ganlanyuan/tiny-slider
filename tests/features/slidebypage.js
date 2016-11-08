c34a5f224c99bbd53a95d83d05b1fb93a4b4e897
William Lin 22 <ganlanyuan@gmail.com>
2016-11-08 16:38:55 -0500

    update slidebypage

--- tests/features/slidebypage.js
+++ tests/features/slidebypage.js
@@ -8,14 +8,51 @@ fixture `slideByPage`
 
 test('slideByPage: init', async t => {
   await t
-    .resizeWindow(windowWidthes[1], windowHeight);
+    .click('.slideByPage_wrapper [data-controls="next"]')
+    .wait(speed1);
 
   // check sliders position
-  var container = await select('#nonLoop');
-  var parent = await container.getParentNode();
-  container = await select('#nonLoop');
-  var slide0 = await container.getChildElement(0);
-  var slide2 = await container.getChildElement(2);
-  expect(Math.round(slide0.boundingClientRect.left)).to.equal(0);
-  expect(Math.round(slide2.boundingClientRect.right)).to.equal(parent.boundingClientRect.right);
+  var innerWidth = await getWindowInnerWidth();
+  var container = await select('#slideByPage');
+  var slide14 = await container.getChildElement(14);
+  var slide17 = await container.getChildElement(17);
+  expect(Math.round(slide14.boundingClientRect.left)).to.equal(0);
+  expect(Math.round(slide17.boundingClientRect.right)).to.equal(innerWidth);
+  
+  await t
+    .click('.slideByPage_wrapper [data-controls="next"]')
+    .wait(speed1);
+
+  var slide8 = await container.getChildElement(8);
+  var slide11 = await container.getChildElement(11);
+  expect(Math.round(slide8.boundingClientRect.left)).to.equal(0);
+  expect(Math.round(slide11.boundingClientRect.right)).to.equal(innerWidth);
+
+  await t
+    .click('.slideByPage_wrapper [data-controls="next"]')
+    .wait(speed1);
+
+  var slide12 = await container.getChildElement(12);
+  var slide15 = await container.getChildElement(15);
+  expect(Math.round(slide12.boundingClientRect.left)).to.equal(0);
+  expect(Math.round(slide15.boundingClientRect.right)).to.equal(innerWidth);
+
+  await t
+    .click('.slideByPage_wrapper [data-controls="next"]')
+    .wait(speed1);
+
+  var slide16 = await container.getChildElement(16);
+  var slide19 = await container.getChildElement(19);
+  expect(Math.round(slide16.boundingClientRect.left)).to.equal(0);
+  expect(Math.round(slide19.boundingClientRect.right)).to.equal(innerWidth);
+
+  await t
+    .click('.slideByPage_wrapper [data-controls="next"]')
+    .wait(speed1);
+
+  var slide5 = await container.getChildElement(5);
+  var slide8 = await container.getChildElement(8);
+  expect(Math.round(slide5.boundingClientRect.left)).to.equal(0);
+  expect(Math.round(slide8.boundingClientRect.right)).to.equal(innerWidth);
+
 });
