ff398107e5176998cb9916854b4ea6649427a6d8
William Lin 22 <ganlanyuan@gmail.com>
2017-09-06 11:24:20 -0400

    update getCssRulesLength

--- src/helpers/getCssRulesLength.js
+++ src/helpers/getCssRulesLength.js
@@ -1,9 +1,4 @@
-// cross browsers addRule method
-export var getCssRulesLength = (function () {
-  var styleSheet = document.styleSheets[0];
-  if(styleSheet.cssRules) {
-    return function (sheet) { return sheet.cssRules.length; };
-  } else {
-    return function (sheet) { return sheet.rules.length; };
-  }
-})();
+export function getCssRulesLength(sheet) {
+  var rule = ('insertRule' in sheet) ? sheet.cssRules : sheet.rules;
+  return rule.length;
+}
