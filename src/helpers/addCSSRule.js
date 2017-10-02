557180c06301725f7beec41cc642f00053d54147
Mike Gregory <MeikG@users.noreply.github.com>
2017-10-02 15:55:46 +0100

    resolves #78 bug in addCSSRule when using webpack
    Resolves bug where addCSSRule throws an error upon importing the module. addCSSRule can be executed before the DOM is initialised and document.styleSheets is available. This fix changes addCSSRule to determine styleSheet capabilities at time of execution instead.

--- src/helpers/addCSSRule.js
+++ src/helpers/addCSSRule.js
@@ -1,15 +1,11 @@
 // cross browsers addRule method
 export var addCSSRule = (function () {
-  var styleSheet = document.styleSheets[0];
-  if('insertRule' in styleSheet) {
-
-    return function (sheet, selector, rules, index) {
-      sheet.insertRule(selector + '{' + rules + '}', index);
-    };
-  } else if('addRule' in styleSheet) {
-
-    return function (sheet, selector, rules, index) {
-      sheet.addRule(selector, rules, index);
-    };
+  return function (sheet, selector, rules, index) {
+      var styleSheet = document.styleSheets[0];
+      if('insertRule' in styleSheet) {
+          sheet.insertRule(selector + '{' + rules + '}', index);
+      } else if('addRule' in styleSheet) {
+          sheet.addRule(selector, rules, index);
+      }
   }
 })();
