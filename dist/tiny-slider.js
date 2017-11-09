f841bcee7d5623cc5aef888b918bbf5178ef00ce
William Lin 22 <ganlanyuan@users.noreply.github.com>
2017-11-09 15:58:43 -0500

    update version

--- dist/tiny-slider.js
+++ dist/tiny-slider.js
@@ -410,7 +410,7 @@ function jsTransform(element, attr, prefix, postfix, to, duration, callback) {
 }
 
 // Format: IIFE
-// Version: 2.3.2
+// Version: 2.3.3
 
 // helper functions
 // check browser version and local storage
--- src/tiny-slider.module.js
+++ src/tiny-slider.module.js
@@ -1,5 +1,5 @@
 // Format: ES MODULE
-// Version: 2.3.2
+// Version: 2.3.3
 
 // helper functions
 import './helpers/keys';
