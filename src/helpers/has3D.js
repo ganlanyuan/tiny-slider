43b39215d4dc75d9d1f12e9ce09e71f2845e109d
William Lin 22 <ganlanyuan@users.noreply.github.com>
2018-06-16 17:06:42 -0400

    update has3D.js so that it can run in <head>

--- src/helpers/has3D.js
+++ src/helpers/has3D.js
@@ -1,18 +1,27 @@
+import './childNode.remove';
+import { getBody } from './getBody';
+import { setFakeBody } from './setFakeBody';
+import { resetFakeBody } from './resetFakeBody';
+
 export function has3D(tf){
   if (!window.getComputedStyle) { return false; }
   
-  var el = document.createElement('p'),
+  var doc = document,
+      body = getBody(),
+      docOverflow = setFakeBody(body),
+      el = doc.createElement('p'),
       has3d,
       cssTF = tf.length > 9 ? '-' + tf.slice(0, -9).toLowerCase() + '-' : '';
 
   cssTF += 'transform';
 
   // Add it to the body to get the computed style
-  document.body.insertBefore(el, null);
+  body.insertBefore(el, null);
 
   el.style[tf] = 'translate3d(1px,1px,1px)';
   has3d = window.getComputedStyle(el).getPropertyValue(cssTF);
 
-  document.body.removeChild(el);
+  body.fake ? resetFakeBody(body, docOverflow) : el.remove();
+
   return (has3d !== undefined && has3d.length > 0 && has3d !== "none");
 }
