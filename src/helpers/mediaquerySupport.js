df4b62bd79efc305ac18f78e4547007eee2bbd71
Nghiệp <me@nghiepit.dev>
2020-04-02 00:05:34 +0700

    Fix mediaquerySupport helper
    The original helper doesn't work in Safari, cause many side-effects.
    See #504
    Use javascript native to detect media query support, before using the old way.

--- src/helpers/mediaquerySupport.js
+++ src/helpers/mediaquerySupport.js
@@ -3,6 +3,10 @@ import { setFakeBody } from './setFakeBody.js';
 import { resetFakeBody } from './resetFakeBody.js';
 
 export function mediaquerySupport () {
+  if (window.matchMedia || window.msMatchMedia) {
+    return true;
+  }
+  
   var doc = document,
       body = getBody(),
       docOverflow = setFakeBody(body),
