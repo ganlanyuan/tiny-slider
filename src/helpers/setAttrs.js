c5e126c3d3f7363105174e7339a383f827e54807
Viktor <livion71@gmail.com>
2020-04-14 03:17:08 +0300

    els[i].setAttribute is not a function
    options: {
            "items": 3,
            "controlsContainer": "#customize-controls",
            "navContainer": "#customize-thumbnails",
            "navAsThumbnails": true,
            "autoplay": true,
            "autoplayTimeout": 1000,
            "autoplayButton": "#customize-toggle",
            "swipeAngle": false,
            "speed": 400
    }

--- src/helpers/setAttrs.js
+++ src/helpers/setAttrs.js
@@ -6,6 +6,19 @@ export function setAttrs(els, attrs) {
 
   for (var i = els.length; i--;) {
     for(var key in attrs) {
+      // Create HTMLDOMElemnt from string value
+      if(typeof els[i] === 'string') {
+        let attr = els[i].substr(0, 1);
+        let attrValue = els[i].substr(1);
+        els[i] = document.createElement('div');
+        
+        if(attr === '#') {
+          els[i].setAttribute('id', attrValue);
+        } else {
+          els[i].setAttribute('class', attrValue.slit('.').join(' '));
+        }
+      }
+      
       els[i].setAttribute(key, attrs[key]);
     }
   }
